93e9ed8b792264bf2f2c0a5d36c3491c7f5bbee7
MarkwSch <131507173+MarkwSch@users.noreply.github.com>
2023-12-12 16:41:47 +0800

    fix: first correction final project

--- assets/js/hamburgermenu.js
+++ assets/js/hamburgermenu.js
@@ -1,12 +1,17 @@
 function toggleDropdown() {
     var dropdownMenu = document.getElementById("dropdownMenu");
-    var hamburgerButton = document.querySelector(".hamburger-button");
+    var hamburgerButton = document.querySelector(".hamburgermenu");
 
-    if (dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none";
-    } else {
-        dropdownMenu.style.display = "block";
+    if (dropdownMenu) { // Basically, if it exists, e.g not null.
+        if (dropdownMenu.style.display === "block") {
+            dropdownMenu.style.display = "none";
+        } else {
+            dropdownMenu.style.display = "block";
+        }
     }
 
-    hamburgerButton.style.display = "block";
+    if (hamburgerButton) {
+        hamburgerButton.style.display = "block";
+    }
+
 }
